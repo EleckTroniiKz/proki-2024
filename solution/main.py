f471ecaf076b57b64874641f174a9747cc81c8c1
ujhun <ujhun@student.kit.edu>
2024-12-16 20:14:53 +0100

    test

--- solution/main.py
+++ solution/main.py
@@ -7,13 +7,6 @@ from rich.progress import track
 import pandas as pd
 
 from algorithm import findCenterOfGripper
-from solution.algorithm import findCenterOfGripper
-
-def create_part_mask(part_image_path: Path, invert_mask: bool = False, blur_method = "median", adaptive = True, area_filter = True, show_images = False) -> np.ndarray:
-    # Load the image
-    img = cv2.imread(str(part_image_path), cv2.IMREAD_COLOR)
-    if img is None:
-        raise ValueError(f"Could not read the image from {part_image_path}")
 
 
 
@@ -22,15 +15,11 @@ def compute_amazing_solution(
     part_image_path: Path, gripper_image_path: Path
 ) -> tuple[float, float, float]:
     print(part_image_path)
-    coords = run_algorithm(part_image_path, gripper_image_path)
+    coords = findCenterOfGripper(part_image_path, gripper_image_path)
     if coords is None:
         print(f"Kein Schwerpunkt für {part_image_path}. Zeile überspringen.")
         return None, None, None
 
-
-    a = create_part_mask(part_image_path)
-
-    return 100.1, 95, 91.2
     return coords
 
 
