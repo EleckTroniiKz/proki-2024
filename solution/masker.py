cb26a888240268e989bbd1422c85adaf22231e75
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-12-28 15:56:41 +0100

    Masker code (still has artifacts, but it is acceptable with some examples)

--- solution/masker.py
+++ solution/masker.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from PIL import Image
 import os
 # Load the image
 # Load the first provided part image for processing
@@ -13,7 +12,7 @@ def create_part_mask(part_image_path):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     equalized_image = clahe.apply(part_image)
 
-    tile_size = 100
+    tile_size = 100  #tile size 100x100
     h, w = equalized_image.shape
     result_mask = np.zeros_like(equalized_image)
 
@@ -27,25 +26,15 @@ def create_part_mask(part_image_path):
 
             result_mask[y:y+tile_thresh.shape[0], x:x+tile_thresh.shape[1]] = tile_thresh
 
-
-
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(result_mask, connectivity=8)
     filtered_mask = np.zeros_like(result_mask)
 
-    min_area = 100
+    min_area = 100  # Minimum area to retain a component
     for i in range(1, num_labels):  # Skip the background label
         if stats[i, cv2.CC_STAT_AREA] >= min_area:
             filtered_mask[labels == i] = 255
 
-    output_dir = "_masks"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    output_path = os.path.join("_masks", f"{os.path.basename(part_image_path)}_output.png")
-    print(part_image_path)
-
+    output_path = os.path.join("_masks", f"{part_image_path}_output.png")
     cv2.imwrite(output_path, filtered_mask)
 
-    pil_image = Image.fromarray(filtered_mask)
-
-    return pil_image
+    return filtered_mask
